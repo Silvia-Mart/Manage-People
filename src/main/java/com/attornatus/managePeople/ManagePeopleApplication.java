930309f571fa61f7977c1198564c3331e8ea9819
Silvia-Mart <santosmartinssilvia@gmail.com>
2023-02-16 10:10:41 -0300

    init project

---
+++ src/main/java/com/attornatus/managePeople/ManagePeopleApplication.java
@@ -0,0 +1,13 @@
+package com.attornatus.managePeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagePeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagePeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attornatus/managePeople/ManagePeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.managePeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagePeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
